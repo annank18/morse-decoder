fe5c847074315438d16c1cda080f0f185c4ff7a5
Anna <annank18@gmail.com>
2022-02-23 06:19:40 +0400

    feat: add morse function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,50 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let Arrays = [];
+  let morse =[];
+  let morse2 =[];
+  let morse3 = [];
+  for (let i = 0; i < expr.length; i += 2) {
+    Arrays.push(expr.slice(i, i+2));
+  }
+
+Arrays.forEach(item => {
+  if (item === '10') {
+    item = '.';
+  } else if (item === '11') {
+    item = '-';
+  } else if (item === '00') {
+    item = '';
+  }
+  morse.push(item);
+})
+ 
+for (let i = 0; i < morse.length; i += 5) {
+  morse2.push(morse.slice(i, i+5));
+}
+
+for (let i = 0; i < morse2.length; i++) {
+  morse = morse2[i].filter(item => item != '' && item != '**');
+  morse3.push(morse);
+}
+
+morse2 = [];
+
+for (let i = 0; i < morse3.length; i++) {
+  morse = morse3[i].join('');
+  morse2.push(morse);
+}
+
+let res = morse2.map(b => {
+  if (MORSE_TABLE[b] === undefined) {
+    return b = ' ';
+  } else {
+    return MORSE_TABLE[b];
+  }
+  
+}).join('');
+return res;
 }
 
 module.exports = {
